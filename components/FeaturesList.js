c46f83af0de2046803ee5d8780da32b0772a1b7b
ObywanKenoby309 <ericwjames309@gmail.com>
2025-08-04 18:52:30 +0300

    Create FeaturesList.js

---
+++ components/FeaturesList.js
@@ -0,0 +1,14 @@
+// components/FeaturesList.js
+import React from 'react';
+
+export default function FeaturesList({ features = [] }) {
+  return (
+    <ul className="max-w-xl mx-auto list-disc list-inside space-y-4 text-lg leading-relaxed">
+      {features.map((feature, index) => (
+        <li key={index} className="text-[#ff4500] font-semibold">
+          {feature}
+        </li>
+      ))}
+    </ul>
+  );
+}
