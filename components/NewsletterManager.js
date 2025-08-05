8a2efa08791a85233e1864136f0051489e2b48ec
ObywanKenoby309 <ericwjames309@gmail.com>
2025-08-05 16:17:52 +0300

    Create NewsletterManager.js

---
+++ components/NewsletterManager.js
@@ -0,0 +1,8 @@
+export default function NewsletterManager() {
+  return (
+    <section className="bg-white rounded-lg shadow p-6">
+      <h2 className="text-2xl font-semibold text-[#FF7043] mb-4">Newsletter Manager</h2>
+      <p className="text-gray-600">Create and send newsletters to your mentees and followers here.</p>
+    </section>
+  );
+}
