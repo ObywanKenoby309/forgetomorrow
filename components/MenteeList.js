aff92202fb71cf5671005a465a2f508850bb9744
ObywanKenoby309 <ericwjames309@gmail.com>
2025-08-05 16:10:09 +0300

    Create MenteeList.js

---
+++ components/MenteeList.js
@@ -0,0 +1,8 @@
+export default function MenteeList() {
+  return (
+    <section className="bg-white rounded-lg shadow p-6">
+      <h2 className="text-2xl font-semibold text-[#FF7043] mb-4">Mentees</h2>
+      <p className="text-gray-600">List of your mentees will appear here. Manage progress and communication.</p>
+    </section>
+  );
+}
