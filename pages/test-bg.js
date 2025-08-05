b6f4f32232580936d39fc901c489abd5d0b99bbb
ObywanKenoby309 <ericwjames309@gmail.com>
2025-08-05 21:09:23 +0300

    Create test-bg.js

---
+++ pages/test-bg.js
@@ -0,0 +1,20 @@
+export default function TestBG() {
+  return (
+    <div
+      style={{
+        backgroundImage: "url('/images/forge-bg-bw.png')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        width: '100%',
+        color: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '2rem',
+      }}
+    >
+      Background Test
+    </div>
+  );
+}
