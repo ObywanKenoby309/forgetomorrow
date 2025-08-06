fab353a2f4751a4f1c53d00b5db2f5bfc17907e9
ObywanKenoby309 <ericwjames309@gmail.com>
2025-08-06 10:43:36 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,31 @@
+// pages/_app.js
 import '../styles/globals.css';
+import { useRouter } from 'next/router';
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import LandingHeader from '../components/LandingHeader';
+import LandingFooter from '../components/LandingFooter';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      {/* Full viewport background with faded overlay */}
-      <div
-        style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          height: '100vh',
-          width: '100vw',
-          backgroundImage: "url('/forge-bg-bw.png')",
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          filter: 'brightness(0.4)',
-          zIndex: -1,
-        }}
-      />
+  const router = useRouter();
+  const isLandingPage = ['/', '/signup', '/features'].includes(router.pathname);
 
-      <Header />
+  return (
+    <div
+      className="relative min-h-screen"
+      style={{
+        backgroundImage: "url('/images/forge-bg-bw.png')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundAttachment: 'fixed',
+      }}
+    >
+      {isLandingPage ? <LandingHeader /> : <Header />}
 
       <Component {...pageProps} />
 
-      <Footer />
-    </>
+      {isLandingPage ? <LandingFooter /> : <Footer />}
+    </div>
   );
 }
