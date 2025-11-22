1186c743bf349611e06ddec649cbca85428e783e
ObywanKenoby309 <ericwjames309@gmail.com>
2025-11-22 23:53:30 +0200

    Update _app.js with new changes

--- pages/_app.js
+++ pages/_app.js
@@ -99,7 +99,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Forget Tomorrow</title>
+        <title>ForgeTomorrow</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
