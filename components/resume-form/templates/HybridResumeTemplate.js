11b3089d75a51b5aad3b10d0cd9e783fe813ef10
ObywanKenoby309 <ericwjames309@gmail.com>
2025-12-07 19:30:40 +0200

    updated layout of hybrid resume template

--- components/resume-form/templates/HybridResumeTemplate.js
+++ components/resume-form/templates/HybridResumeTemplate.js
@@ -83,7 +83,7 @@ export default function HybridResumeTemplate({ data }) {
               fontSize: '13pt',
               fontWeight: 'bold',
               marginBottom: '6pt',
-              borderBottom: '1pt solid '#000',
+              borderBottom: '1pt solid #000',
               textTransform: 'uppercase',
             }}
           >
