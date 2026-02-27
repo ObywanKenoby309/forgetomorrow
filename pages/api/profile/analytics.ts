26dc67989b79f152bdf3c1e53274b7def2b7faf4
ObywanKenoby309 <ericwjames309@gmail.com>
2026-02-28 00:25:05 +0200

    Updating authOptions in analytics to solve deployment error

--- pages/api/profile/analytics.ts
+++ pages/api/profile/analytics.ts
@@ -1,7 +1,7 @@
 // pages/api/profile/analytics.ts
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
-import authOptions from "../auth/[...nextauth]";
+import { authOptions } from "../auth/[...nextauth]";
 import { prisma } from "@/lib/prisma";
 import jwt from "jsonwebtoken";
 
