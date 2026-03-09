5340ff03081cf53d55cf8893c14b3a467b52ec40
ObywanKenoby309 <ericwjames309@gmail.com>
2026-03-09 05:02:30 +0200

    New public slug layout

--- pages/u/[slug].js
+++ pages/u/[slug].js
@@ -172,9 +172,9 @@ export default function PublicProfile({ user, primaryResume, effectiveVisibility
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@500;600;700;800&family=Outfit:wght@300;400;500;600;700&display=swap"
-rel="stylesheet"
-/>
+          href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=Outfit:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <style>{`
